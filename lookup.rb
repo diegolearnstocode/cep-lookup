3b82981135295b582008fcfd998f354961684252
Diego van Dyk <diegolearnstocode@gmail.com>
2019-04-02 11:25:23 -0300

    added form validation

--- lookup.rb
+++ lookup.rb
@@ -7,7 +7,7 @@ def get_values(json, cep_wanted)
   cep_wanted = cep_wanted.to_i
   json.each do |range|
     range.find do |k,v|
-      if range["weight_from"] == "0,010" && range["cep_from"] < cep_wanted && range["cep_to"] > cep_wanted
+      if range["weight_from"] == "0,010" && range["cep_from"] <= cep_wanted && range["cep_to"] >= cep_wanted
         range_wanted << range
       end
     end
@@ -22,18 +22,22 @@ get '/' do
 end
 
 post '/' do
-  request_uri = "https://raw.githubusercontent.com/diegolearnstocode/cep_lookup/master/base.json"
-  request_query = ''
-  url = "#{request_uri}#{request_query}"
-  buffer = open(url).read
-  json = JSON.parse(buffer)
-  cep = params["cep"]
-  @values = get_values(json,cep)
-  time_from_table = @values["time"]
-  @city = @values["city"]
-  time = time_from_table + 2
-  @time = time.to_s
-  erb :index
+  if params["cep"] == ""
+      erb :index
+  else
+    request_uri = "https://raw.githubusercontent.com/diegolearnstocode/cep_lookup/master/base.json"
+    request_query = ''
+    url = "#{request_uri}#{request_query}"
+    buffer = open(url).read
+    json = JSON.parse(buffer)
+    cep = params["cep"]
+    @values = get_values(json,cep)
+    time_from_table = @values["time"]
+    @city = @values["city"]
+    time = time_from_table + 2
+    @time = time.to_s
+    erb :index
+  end
 end
 
 #<!--<%= @values.nil? ? "Nothing here" : "#{@time} days to #{@city}" =%>-->
